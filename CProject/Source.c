9bb2eaa189e12bb9e3d84a1a748ea867fd49fade
JiHwanNo <83812895+JiHwanNo@users.noreply.github.com>
2023-04-11 21:17:17 +0900

    (2023.04.11) C 프로젝트 생성

---
+++ CProject/Source.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // stdio.h : 기본 입출력 헤더파일
+
+void main() 
+{
+	printf("안녕하세요");
+}
+
+/*
+Github-레파지토리(new) <= 프로젝트가 만들어짐, commit->push 할수있음
+
+git
+gitBash
+SourceTree
+Github Desktop : file - options - 로그인, clone레파지토리
+	change : 프로젝트 업데이트 내역 올라옴
+*/
