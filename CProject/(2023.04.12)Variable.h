aeb5b8b61cdbd910750748f0768664b838882e14
Taeri-jin <wlsl5238@naver.com>
2023-04-13 21:48:30 +0900

    (2023.04.13) 비트와 비트 연산자

--- CProject/(2023.04.12)Variable.h
+++ CProject/(2023.04.12)Variable.h
@@ -1,1 +1,125 @@
 #pragma once
+#include <stdio.h> // stdio.h : 기본 입출력 헤더파일
+
+void main() { // <- void main 하나만 존재해야 함
+#pragma region github
+	/*
+	Github-레파지토리(new) <= 프로젝트가 만들어짐, commit->push 할수있음
+
+	git
+	gitBash
+	SourceTree
+	Github Desktop : file - options - 로그인, clone레파지토리
+		change : 프로젝트 업데이트 내역 올라옴
+		history에서도 변경된 사항 확인할 수 있음
+		push 후 Fetch origin은 github에 업로드 완료
+		github에서 다운로드 할 때 로그인 확인 후 clone으로 다운받은 후 파일에서 .sln파일 실행
+	*/
+#pragma endregion
+
+#pragma region 변수 
+	// pragma region 사용시 닫기했을때 리전 이름이 뜸, 주석필수
+	/*
+	변수란 ?
+	데이터를 저장할 수 있는 메모리 공간을 생성한는 것
+	*/
+
+	/*
+	자료형이란 ?
+	데이터를 저장하기 위한 데이터의 형태를 정해주는 것
+	*/
+
+	/*
+	자료형
+	char : 한 문자를 저장하기 위한 자료형 (1byte)
+	short : 정수를 저장하기 위한 자료형 (2byte)
+	int : 정수를 저장하기 위한 자료형 (4byte)
+	long : 정수 저장 (4byte)
+	float : 실수 저장 (4byte)
+	double : 실수 저장 (8byte)
+	long double : (8~16 byte)
+	*/
+
+	// 자료형 + 변수의 이름 , ;(세미콜론) : 한 문장의 끝을 알려주는 기호
+	/*
+	char alphabet = 'A'; // 디버깅 화살표가 여기 있다면 디버깅 실행 상태
+
+	int data = 100;
+
+	float pi = 3.14f; //f안적으면 double로 잡히게 됨
+	*/
+#pragma endregion
+
+	//변수의 이름 규칙
+	/*
+	1. 예약어를 사용할 수 없음 ( ex. int int = 100; )
+	2. 숫자를 변수이름으로 사용할수 있으나 첫번째 단어로 사용할 수 없음 ( ex. int hp1 = 300;(o), int 1hp = 200;(x) )
+	3. 변수의 이름은 대소문자를 구분함 ( ex. int attack, int ATTACK <= 서로 다른 변수이며 메모리를 가짐 )
+	4. 변수의 이름에 공백이 포함될 수 없음 ( ex. int count Down;(x) )
+	5. 변수의 이름으로 특수 기호는 "$", "_"만 가능 ( ex. int count_Down = 5;(o), int mo$ney = 999;(o)
+	*/
+
+	// 한 라인 선택할시 해당 라인에서 alt 누르고 밑으로 드래그
+
+	//printf("안녕하세요");
+
+	// 컴파일러 : 사람(10진수)과 컴퓨터(2진수) 사이에서 번역
+	// 런타임 : 실행중에 오류. 소스코드 분석
+
+#pragma region 변수의 초기화
+	// 최초로 변수에 값을 저장해주는 과정
+	int exp = 100; // 변수 선언
+	exp = 999;
+#pragma endregion
+
+#pragma region 상수
+	// 프로그램이 실행되는 동안 더 이상 값을 변경할 수 없는 메모리 공간임
+	// 한 번 값을 초기화하게 되면 더 이상 값을 변경할 수 없음
+
+	//const float pi = 3.141592;
+	// pi = 6.4571; <= 상수로 선언되었기 때문에 값을 변경할 수 없음
+	//L-value와 R-value
+
+	/*
+	심볼릭상수 : 메모리 공간을 가지고 있는 상수
+
+	리터럴 상수 : 메모리 공간을 안 가지고 있는 상수. 순수 값 그 자체 ( ex. 3.141592 )
+	*/
+#pragma endregion
+
+#pragma region 산술 연산자
+	//int result = (10 - 2) * 3;
+
+	int num1;
+	int b = 15;
+	int c = 50;
+
+	int num2;
+	int e = 30;
+	const int f = 17;
+
+	int num3;
+
+	int num4;
+
+	int num5;
+
+	// 1. num1 변수 = B 변수 + C 변수
+	num1 = b + c;
+	printf("num1 = %d\n", num1);
+	// 2. num2 변수 = E 변수 + F (심볼릭)상수
+	num2 = e + f;
+	printf("num2 = %d\n", num2);
+	// 3. num3 변수 = 리터럴 상수 * B 변수
+	num3 = 17 * b;
+	printf("num3 = %d\n", num3);
+	// 4. num4 변수 = 리터럴 상수 / F (심볼릭)상수
+	num4 = 17 / f;
+	printf("num4 = %d\n", num4);
+	// 5. num5 변수 = 리터럴 상수 + 리터럴 상수
+	num5 = 23 + 27;
+	printf("num5 = %d\n", num5);
+
+#pragma endregion
+}
+
--- CProject/Source.c
+++ CProject/Source.c
@@ -1,123 +1,68 @@
-#include <stdio.h> // stdio.h : 기본 입출력 헤더파일
+#include <stdio.h>
 
-void main() { // <- void main 하나만 존재해야 함
-#pragma region github
-/*
-Github-레파지토리(new) <= 프로젝트가 만들어짐, commit->push 할수있음
-
-git
-gitBash
-SourceTree
-Github Desktop : file - options - 로그인, clone레파지토리
-	change : 프로젝트 업데이트 내역 올라옴
-	history에서도 변경된 사항 확인할 수 있음
-	push 후 Fetch origin은 github에 업로드 완료
-	github에서 다운로드 할 때 로그인 확인 후 clone으로 다운받은 후 파일에서 .sln파일 실행
-*/
-#pragma endregion
-
-#pragma region 변수 
-	// pragma region 사용시 닫기했을때 리전 이름이 뜸, 주석필수
+void main() {
+#pragma region 서식 지정자
 	/*
-	변수란 ?
-	데이터를 저장할 수 있는 메모리 공간을 생성한는 것
-	*/
+	출력하기 위한 자료형의 정보를 명시적으로 지정해주는 것
+	
+	"%c" 문자 ( 서식 지정자 )
+	"%d" 정수 ( 서식 지정자 )
+	"%f" 실수 ( 서식 지정자 float )
+	
 
-	/*
-	자료형이란 ?
-	데이터를 저장하기 위한 데이터의 형태를 정해주는 것
-	*/
-
-	/*
-	자료형
-	char : 한 문자를 저장하기 위한 자료형 (1byte)
-	short : 정수를 저장하기 위한 자료형 (2byte)
-	int : 정수를 저장하기 위한 자료형 (4byte)
-	long : 정수 저장 (4byte)
-	float : 실수 저장 (4byte)
-	double : 실수 저장 (8byte)
-	long double : (8~16 byte)
-	*/
-
-	// 자료형 + 변수의 이름 , ;(세미콜론) : 한 문장의 끝을 알려주는 기호
-	/*
-	char alphabet = 'A'; // 디버깅 화살표가 여기 있다면 디버깅 실행 상태
-
+	char character = 'B';
 	int data = 100;
+	float decimal = 9.953111111f;
 
-	float pi = 3.14f; //f안적으면 double로 잡히게 됨
+	// printf(data); (x)
+	// 서식과 변수의 형태가 일치하지 않으면 원하는 값이 출력되지 않음
+	//printf("character의 값 : %f\n", character); (x)
+	printf("character의 값 : %c\n", character);
+	printf("data의 값 : %d\n", data);
+
+	// float 4byte 크기의 실수형 자료형은 소수점 이하 6자리까지만 표기함
+	printf("decimal의 값 : %f\n", decimal);
+
+	// 서식 지정자의 순서는 왼쪽에서부터 시작함
+	printf("data : %d, decimal : %f", data, decimal);
 	*/
 #pragma endregion
+	
+#pragma region 비트(bit)
+	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의 조합으로 논리 계산을 수행하는 단위
+	// bit가 8개 모이면 1byte가 됨
 
-	//변수의 이름 규칙
-	/*
-	1. 예약어를 사용할 수 없음 ( ex. int int = 100; )
-	2. 숫자를 변수이름으로 사용할수 있으나 첫번째 단어로 사용할 수 없음 ( ex. int hp1 = 300;(o), int 1hp = 200;(x) )
-	3. 변수의 이름은 대소문자를 구분함 ( ex. int attack, int ATTACK <= 서로 다른 변수이며 메모리를 가짐 )
-	4. 변수의 이름에 공백이 포함될 수 없음 ( ex. int count Down;(x) )
-	5. 변수의 이름으로 특수 기호는 "$", "_"만 가능 ( ex. int count_Down = 5;(o), int mo$ney = 999;(o)
+	/* 10진수 -> 2진수
+	10진수를 1이 될때까지 계속 2로 나누어 준 다음 나눈 위치의 나머지 값을 아래에서 위의 순서로 정렬
 	*/
 
-	// 한 라인 선택할시 해당 라인에서 alt 누르고 밑으로 드래그
-	
-	//printf("안녕하세요");
-
-	// 컴파일러 : 사람(10진수)과 컴퓨터(2진수) 사이에서 번역
-	// 런타임 : 실행중에 오류. 소스코드 분석
-	
-#pragma region 변수의 초기화
-	// 최초로 변수에 값을 저장해주는 과정
-	int exp = 100; // 변수 선언
-	exp = 999;
-#pragma endregion
-
-#pragma region 상수
-	// 프로그램이 실행되는 동안 더 이상 값을 변경할 수 없는 메모리 공간임
-	// 한 번 값을 초기화하게 되면 더 이상 값을 변경할 수 없음
-	
-	//const float pi = 3.141592;
-	// pi = 6.4571; <= 상수로 선언되었기 때문에 값을 변경할 수 없음
-	//L-value와 R-value
+	/* 2진수 -> 10진수
+	*/
 
 	/*
-	심볼릭상수 : 메모리 공간을 가지고 있는 상수
-	
-	리터럴 상수 : 메모리 공간을 안 가지고 있는 상수. 순수 값 그 자체 ( ex. 3.141592 )
+	비트 연산자
+	비트 단위로 논리 연산을 수행하기 위해 사용하는 연산자
 	*/
-#pragma endregion
-
-#pragma region 산술 연산자
-	//int result = (10 - 2) * 3;
-
-	int num1;
-	int b = 15;
-	int c = 50;
-
-	int num2;
-	int e = 30;
-	const int f = 17;
-
-	int num3;
-
-	int num4;
+	// AND : 두 개의 피연산자 모두 1이면 1을 반환하는 연산자
+	int num1 = 10; // 00001010
+	int num2 = 15; // 00001111
+	char num3 = 10;
+	char num4 = 15;
+	char num5 = 129;
 	
-	int num5;
+	printf("num1과 num2를 AND 연산한 결과 : %d\n", num1 & num2);
 
-	// 1. num1 변수 = B 변수 + C 변수
-	num1 = b + c;
-	printf("num1 = %d\n", num1);
-	// 2. num2 변수 = E 변수 + F (심볼릭)상수
-	num2 = e + f;
-	printf("num2 = %d\n", num2);
-	// 3. num3 변수 = 리터럴 상수 * B 변수
-	num3 = 17 * b;
-	printf("num3 = %d\n", num3);
-	// 4. num4 변수 = 리터럴 상수 / F (심볼릭)상수
-	num4 = 17 / f;
-	printf("num4 = %d\n", num4);
-	// 5. num5 변수 = 리터럴 상수 + 리터럴 상수
-	num5 = 23 + 27;
-	printf("num5 = %d\n", num5);
+	// OR : 두 개의 피연산자 중에 하나라도 1이 있다면 1을 반환하는 연산자
+	printf("num1과 num2를 OR 연산한 결과 : %d\n", num1 | num2);
+
+	// XOR : 두 개의 피연산자가 서로 같으면 0을 반환하고, 서로 다르면 1을 반환하는 연산자
+	printf("num1과 num2를 XOR 연산한 결과 : %d\n", num1 ^ num2);
+
+	// NOT : 비트를 반전시키는(뒤집는) 연산자
+	printf("num3을 NOT 연산한 결과 : %d\n", ~num3); // -128+64+32+16+8+4+2+1
+	// 첫 번째 비트는 부호를 나타내며, 첫 번째 비트에 1이 있다면 값은 음수 0이 있다면 양수
+	
+	printf("num5의 값 : %d\n", num5); // -128~127만 표현 가능. 오버플로우 현상 발생
 
 #pragma endregion
 }
