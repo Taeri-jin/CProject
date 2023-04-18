79c1d2ff5556a242e383a45b01ef656dfa09ddbe
JiHwanNo <83812895+JiHwanNo@users.noreply.github.com>
2023-04-18 21:56:00 +0900

    (2023.04.18) 포인터

---
+++ CProject/(2023.04.17)반복문.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <stdio.h>
+
+void main()
+{
+# pragma region switch문
+	/*
+	// 어떤 결과에 따라 그 결과부터 실행되는 명령문
+
+	int count = 3;
+
+	// switch문의 경우 조건에 해당하는 값에 따라 조건의 위치로 이동
+	// 선택지에서 많이 사용됨
+	switch(count) // <- 값
+	{
+		// break문이란?
+		// 특정한 지점에서 분기를 탈출하는 제어문
+		case 0 : printf("0입니다.\n");
+			break;
+		case 1 : printf("1입니다.\n");
+			break;
+		case 2 : printf("2입니다.\n");
+			break;
+		default: printf("예외적인 값이 발생했습니다.\n");
+			break;
+	}
+	*/
+#pragma endregion
+
+#pragma region 증감 연산자
+	/*
+	// 피연산자를 하나씩 증가시키거나 감소시킬 때 사용하는 연산자
+
+	// 전위 증감 연산자
+	// 변수의 값을 증감시킨 후에 연산을 수행하는 연산자
+	int a = 0;
+	int result1 = 0;
+
+	result1 = ++a; // 증가
+	printf("result1의 값 : %d\n", result1);
+
+	result1 = --result1; // 감소
+	printf("result1의 값 : %d\n", result1);
+
+	// 후위 증감 연산자
+	// 연산을 수행 한 다음 변수의 값을 증감시키는 연산자
+
+	int b = 0;
+	int result2 = 0;
+
+	result2 = b++; // 증감
+	printf("result2의 값 : %d\n", result2);
+	printf("%d\n", b); // b(1)
+
+	result2 = b--; // 감소
+	printf("result2의 값 : %d\n", result2);
+	printf("%d\n", b); 	// b(0)
+	*/
+#pragma endregion
+
+#pragma region 반복문(for문)
+	/*
+	// 초기식을 연산하여 조건식의 결과에 따라 특정한 횟수만큼 반복하는 반복문
+	//      초기식    조건식  증감식
+	for (int i = 0 ; i < 10 ; i++) // 조건이 거짓이 될때까지 계속 반복
+	{
+		printf("게임 중...\n"); // 반복할 내용
+	}
+
+	// 반복문 문제
+	// 반복문으로 초기값을 1 ~ 5의 값을 출력합니다.
+	for (int i = 1 ; i <= 5 ; i++)
+	{
+		// for문의 경우 조건이 끝나는 형태와 반대로 초기식을 연산하게 되면 조건이 일치하지 않아 계속 반복적으로 실행되는 문제가 발생 = 무한루프
+		printf("i의 값 : %d\n", i);
+	}
+
+
+	// 반복문 문제
+	// 1 ~ 10 까지의 합을 구해주세요.
+	int result = 0;
+	for (int i = 1 ; i <= 10 ; i++)
+	{
+		result += i;
+		// 복합 대입 연산자 : +=, -=, *=, /=
+	}
+	printf("%d\n", result);
+	*/
+
+	// 2중 for문
+	/*
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("안쪽에 있는 for문\n");
+		}
+		printf("바깥에 있는 for문\n");
+	}
+	*/
+#pragma endregion
+
+
+
+
+
+}
--- CProject/Source.c
+++ CProject/Source.c
@@ -2,103 +2,120 @@
 
 void main()
 {
-# pragma region switch문
+#pragma region 반복문 (while)
+	// 특정 조건을 거짓이 될 때까지 계속해서 주어진 명령문을 실행하는 반복문
 	/*
-	// 어떤 결과에 따라 그 결과부터 실행되는 명령문
+	int count = 5;
 
-	int count = 3;
-
-	// switch문의 경우 조건에 해당하는 값에 따라 조건의 위치로 이동
-	// 선택지에서 많이 사용됨
-	switch(count) // <- 값
+	while (count) // <- 조건
 	{
-		// break문이란?
-		// 특정한 지점에서 분기를 탈출하는 제어문
-		case 0 : printf("0입니다.\n");
-			break;
-		case 1 : printf("1입니다.\n");
-			break;
-		case 2 : printf("2입니다.\n");
-			break;
-		default: printf("예외적인 값이 발생했습니다.\n");
-			break;
+		printf("게임 실행...\n");
+		count--;
 	}
 	*/
+
+
+
+
 #pragma endregion
 
-#pragma region 증감 연산자
+#pragma region continue 문
+	// 해당 조건문만 실행하지 않고, 반복문은 이어서 실행하는 제어문
 	/*
-	// 피연산자를 하나씩 증가시키거나 감소시킬 때 사용하는 연산자
-
-	// 전위 증감 연산자
-	// 변수의 값을 증감시킨 후에 연산을 수행하는 연산자
-	int a = 0;
-	int result1 = 0;
-
-	result1 = ++a; // 증가
-	printf("result1의 값 : %d\n", result1);
-
-	result1 = --result1; // 감소
-	printf("result1의 값 : %d\n", result1);
-
-	// 후위 증감 연산자
-	// 연산을 수행 한 다음 변수의 값을 증감시키는 연산자
-
-	int b = 0;
-	int result2 = 0;
-
-	result2 = b++; // 증감
-	printf("result2의 값 : %d\n", result2);
-	printf("%d\n", b); // b(1)
-
-	result2 = b--; // 감소
-	printf("result2의 값 : %d\n", result2);
-	printf("%d\n", b); 	// b(0)
-	*/
-#pragma endregion
-
-#pragma region 반복문(for문)
-	/*
-	// 초기식을 연산하여 조건식의 결과에 따라 특정한 횟수만큼 반복하는 반복문
-	//      초기식    조건식  증감식
-	for (int i = 0 ; i < 10 ; i++) // 조건이 거짓이 될때까지 계속 반복
+	for (int i = 1; i <= 5; i++)
 	{
-		printf("게임 중...\n"); // 반복할 내용
-	}
-
-	// 반복문 문제
-	// 반복문으로 초기값을 1 ~ 5의 값을 출력합니다.
-	for (int i = 1 ; i <= 5 ; i++)
-	{
-		// for문의 경우 조건이 끝나는 형태와 반대로 초기식을 연산하게 되면 조건이 일치하지 않아 계속 반복적으로 실행되는 문제가 발생 = 무한루프
-		printf("i의 값 : %d\n", i);
-	}
-	
-
-	// 반복문 문제
-	// 1 ~ 10 까지의 합을 구해주세요.
-	int result = 0;
-	for (int i = 1 ; i <= 10 ; i++)
-	{
-		result += i;
-		// 복합 대입 연산자 : +=, -=, *=, /=
-	}
-	printf("%d\n", result);
-	*/
-
-	// 2중 for문
-	for (int i = 0; i < 3; i++) 
-	{
-		for (int j = 0; j < 3; j++)
+		if (i == 3)
 		{
-			printf("안쪽에 있는 for문\n");
+			// continue;
+			break; // 3일시 반복문 탈출
 		}
-		printf("바깥에 있는 for문\n");
+		printf("%d ", i);
 	}
+	*/
 #pragma endregion
 
+#pragma region 형 변환
+	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이 이루어질 때 기존에 지정했던 자료형을 다른 자료형으로 변환하는 과정
+
+	// 암묵적 형 변환 : 서로 다른 자료형으로 연산이 이루어질 때 자료형의 크기가 큰 자료형으로 변환되는 과정
+	/*
+	int integer = 10;
+	float decimal = 2.5f;
+	
+	// double > float > int > short > char
+	float result = integer + decimal; // integer가 암묵적으로 float 형 변환
+
+	printf("result 변수의 값 : %f\n", result);
+
+	// 명시적 형 변환 : 연산이 이루어지기 전에 사용자가 직접 자료형을 변환하는 과정
+
+	// 프로그래밍에서 정수와 정수끼리의 연산은 정수만 나올수 있음
+	// float 메모리 [2.0f] = 5/2  <= 정수끼리 연산해도 2.0이 나옴
+
+	int a = 5;
+	int b = 2;
+
+	float result1 = (float)a / b; // a는 명시적 형 변환 / 암묵적 형 변환에 따라 b는 float할 필요 없음
+
+	printf("result1의 값 : %f\n", result1); 
+	*/
+#pragma endregion
+
+#pragma region 주소 연산자
+	/*
+	// 변수의 주소 값을 변환하는 연산자(%p, &)
+	// 변수의 주소 값을 출력하게 되면 가장 앞에 있는 시작 주소 값을 출력함
+
+	int data = 333;
+
+	// %p : 데이터의 주소를 출력하는 서식 지정자
+	// x64 : 64비트 (포인터 : 8 Byte)
+	// x86 : 32비트 (포인터 : 4 Byte)
+	// 변수의 주소는 프로그램을 실행할 때마다 계속 바뀜
+	printf("data변수의 주소 : %p\n", &data);
+	*/
+#pragma endregion
+
+#pragma region 포인터
+	// 메모리의 주소 값을 저장할 수 있는 변수
+	// 포인터 변수의 크기는 무조건 64비트에서 8 Byte
+	// 포인터 앞에 자료형은 몇바이트를 읽을지 지정해주기 위해 선언
+	/*
+	int box = 100;
+	int box1 = 999;
+
+	int * ptr = NULL; // int * ptr = &box; <= 선언과 동시에 초기화.
+
+	ptr = &box; // box의 주소 값을 ptr에 저장
+
+	// 포인터 변수도 자신의 메모리 공간을 가지고 있으며, 포인터 변수에 변수의 주소를 저장하게 되면 해당 변수의 시작 주소를 가리킴
+	printf("ptr 변수의 값 : %p\n", ptr);
+	printf("ptr 변수의 주소 값 : %p\n", &ptr);
+	printf("box 변수의 주소 값 : %p\n", &box);
+	printf("box 변수의 값 : %d\n", box);
+
+	// 포인터 변수를 이용해 역참조해서 값을 바꿈
+	*ptr = 600;
+	printf("box 변수의 값 : %d\n", box);
 
 
+	ptr = &box1; // ptr에 box1의 시작 주소값이 들어가 있음
 
+	*ptr = -999; // 역참조
+	printf("box 변수의 값 : %d\n", box);
+	printf("box1 변수의 값 : %d\n", box1);
+
+	double health = 10.5;
+
+	// int ptr1은 4byte의 메모리 공간을 읽음
+	int* ptr1 = &health;
+	printf("ptr1 변수의 값 : %p\n", ptr1);
+
+	// 포인터 변수를 저장하기 위해 주소 값을 저장할 변수의 자료형과 포인터 변수의 자료형이 일치해야 함
+	*ptr1 = 66.75;
+	printf("ptr1 변수가 가리키는 값 : %lf\n", *ptr1); // 쓰레기값?
+	*/
+
+#pragma endregion
 
 }
